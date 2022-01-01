4996992f3480e09c5a3315652ae5a123d0cfe0ee
StudyGO <96668817+studygo-app@users.noreply.github.com>
2022-01-01 21:03:56 +0300

    Add files via upload

---
+++ com/config.js
@@ -0,0 +1,5 @@
+const greet = 'hello world';
+const gotThePass = false;
+const userpicUrl = 'https://studygo-app.github.io/com/userpic.jpg?2';
+const firstName = 'Егор';
+const lastName = 'Горелов';
